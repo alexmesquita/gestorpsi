4336de7e0a5e649082b8fa1a41c100e6005e7882
alexmesquita <alex.mesquita06@hotmail.com>
2014-12-09 11:27:30 -0200

    resolve indentation error

--- fabfile.py
+++ fabfile.py
@@ -7,8 +7,7 @@ def server() :
     env.host_string = '104.131.169.98'
     # your user on that system
     env.user = 'gestorpsi'
-    # Assumes that your *.pem key is in the same directory as your fabfile.py
-    env.key_filename = '104.131.169.98.pem'
+    env.password = 'gestorpsi'
 
 def staging() :
     # path to the directory on the server where your vhost is set up
@@ -23,12 +22,12 @@ def staging() :
         run("git pull origin fabric")
         print(green("Installing requirements..."))
         run("pip install -r requirements.txt")
-	    print(green("Collecting static files..."))
+        print(green("Collecting static files..."))
         run("python manage.py collectstatic --noinput")
         print(green("Syncing the database..."))
         run("python manage.py syncdb")
         print(green("Migrating the database..."))
         run("python manage.py migrate")
         print(green("Restart the uwsgi process"))
-        run("sudo service %s restart" % process)
+        sudo("service %s restart" % process)
     print(red("DONE!"))
